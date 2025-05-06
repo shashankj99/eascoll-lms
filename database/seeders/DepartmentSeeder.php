91ec5c89bb193858cb04c92bb621031d88b0be23
Shashank <shashankj677@gmail.com>
2025-05-06 06:57:31 +0545

    project setup, auth with jetstream

---
+++ database/seeders/DepartmentSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Department;
+
+class DepartmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Department::insert([
+            ['name' => 'B.E. Computer'],
+            ['name' => 'B.E. Civil'],
+            ['name' => 'B.E. Electronics, Communication & Automation'],
+        ]);
+    }
+}
